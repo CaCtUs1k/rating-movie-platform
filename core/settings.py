ab89c1f25db2c8ab14785b02d76a451bd52fed11
CaCtUs1k <denischernish19012004@gmail.com>
2023-10-05 18:16:41 +0300

    Prepare to deploy

--- core/settings.py
+++ core/settings.py
@@ -21,15 +21,14 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = os.environ.get(
-    "DJANGO_SECRET_KEY",
-    "django-insecure-r3-f&-v&^rd@w7-@y!e61j%z*)r^80i+3kemamko7^!$+5nm&p"
-)
+SECRET_KEY = os.environ["DJANGO_SECRET_KEY"]
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = os.environ.get("DJANGO_DEBUG", "") != "False"
+DEBUG = "RENDER" not in os.environ
 
 ALLOWED_HOSTS = ["127.0.0.1"]
+
+
 
 INTERNAL_IPS = [
     "127.0.0.1",
