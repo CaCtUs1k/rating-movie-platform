a21573dc8098fad72c3d3b5020c1b9369a057401
CaCtUs1k <denischernish19012004@gmail.com>
2023-10-16 16:07:55 +0300

    fix avatar's size in profile and readme

--- core/settings.py
+++ core/settings.py
@@ -95,14 +95,6 @@ DATABASES = {
         "ENGINE": "django.db.backends.sqlite3",
         "NAME": BASE_DIR / "db.sqlite3",
     }
-#     'default': {
-#         'ENGINE': 'django.db.backends.postgresql',
-#         'NAME': 'jxwfhanj',
-#         'USER': 'jxwfhanj',
-#         'PASSWORD': 'g1uuIvNe4BimKrkheZj0NW-iEQ0t19ks',
-#         'HOST': 'cornelius.db.elephantsql.com',
-#         'PORT': '5432',
-#     }
 }
 
 db_from_env = dj_database_url.config(conn_max_age=500)
